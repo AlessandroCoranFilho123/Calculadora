f7ff288a5d43f8ccd32570cf1ac32940bf328693
Alessandro Coran Filho <alessandrocoran90@gmail.com>
2025-03-02 14:12:50 -0300

    favicon

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const display = document.getElementById("display");
 
 display.addEventListener("keypress", function (event) {
@@ -44,3 +45,51 @@ function appendValue(value) {
 }
 
 
+=======
+const display = document.getElementById("display");
+
+display.addEventListener("keypress", function (event) {
+    const allowedKeys = "0123456789.+-*/";
+    if (!allowedKeys.includes(event.key)) {
+        event.preventDefault();
+    }
+});
+
+display.addEventListener("input", function () {
+    display.value = display.value.replace("/[^0-9.+\-*/]/g,");
+});
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+        resetOnNextInput = true;
+    } catch {
+        display.value = "Erro";
+        resetOnNextInput = true;
+    }
+}
+
+let resetOnNextInput = false;
+
+function appendValue(value) {
+    if (resetOnNextInput) {
+        display.value = "";
+        resetOnNextInput = false;
+    }
+    display.value += value;
+}
+
+
+>>>>>>> aacf5fc (favicon)
