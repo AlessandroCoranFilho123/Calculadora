015562f2c7d80a983e60fbb764f119d9f6cfd3e2
Alessandro Coran Filho <alessandrocoran90@gmail.com>
2025-03-19 21:26:19 -0300

    modo escuro

--- javascript/script.js
+++ javascript/script.js
@@ -42,3 +42,20 @@ function appendValue(value) {
   }
   display.value += value;
 }
+
+const modeToggle = document.getElementById('mode-toggle');
+const body = document.body;
+
+modeToggle.addEventListener('click', () => {
+    if (body.classList.contains('dark-mode')) {
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
+        modeToggle.textContent = '🌑';
+    } else {
+        body.classList.remove('light-mode');
+        body.classList.add('dark-mode');
+        modeToggle.textContent = '☀️';
+    }
+});
+
+body.classList.add('dark-mode');
